a408b0a3c5aca039b7cf95aa8ed8d913c1f1fd79
CodeWithOmG <omg720944@gmail.com>
2026-04-08 14:23:56 +0530

    UC1

---
+++ App/src/TrainApp.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class TrainApp {
+    public static void main(String[] args){
+        //Display welcome banner
+        System.out.println("====== Train Consist Management App ======");
+
+        List<String> trainConsist = new ArrayList<>();
+
+        System.out.println("Train Initialize Sucessfully");
+    }
+}

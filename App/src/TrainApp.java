a98ba39de59da1ca4cdc5d690b9d92ec4e13f2f6
CodeWithOmG <omg720944@gmail.com>
2026-04-08 15:05:18 +0530

    UC3

--- App/src/TrainApp.java
+++ App/src/TrainApp.java
@@ -1,12 +1,27 @@
-import java.util.*;
+import java.util.HashSet;
 
 public class TrainApp {
-    public static void main(String[] args){
-        //Display welcome banner
-        System.out.println("====== Train Consist Management App ======");
+    public static void main(String[] args) {
 
-        List<String> trainConsist = new ArrayList<>();
+        System.out.println("========================================");
+        System.out.println("UC3 - Track Unique Bogie IDs");
+        System.out.println("========================================\n");
 
-        System.out.println("Train Initialize Sucessfully");
+        HashSet<String> bogieIds = new HashSet<>();
+
+        bogieIds.add("BG101");
+        bogieIds.add("BG102");
+        bogieIds.add("BG103");
+        bogieIds.add("BG104");
+        bogieIds.add("BG102");
+        bogieIds.add("BG101");
+
+        System.out.println("Bogie IDs After Insertion:");
+        System.out.println(bogieIds + "\n");
+
+        System.out.println("Note:");
+        System.out.println("Duplicates are automatically ignored by HashSet.\n");
+
+        System.out.println("UC3 uniqueness validation completed...");
     }
 }
